406ba79f9e64336864d77e37c6fd3d99d8930d6c
josh hart <joshhartbiz@gmail.com>
2020-07-31 09:54:48 +0000

    Update ArgumentParsingTests.cs

--- torque.IntegrationTests.Postgres/ArgumentParsingTests.cs
+++ torque.IntegrationTests.Postgres/ArgumentParsingTests.cs
@@ -18,7 +18,7 @@ namespace torque.IntegrationTests.Postgres
             var sb = new StringBuilder();
             Program.loggingService = new TestLoggingService(sb);
             await Program.Main(new string[] { "help" });
-            Assert.AreEqual("torque Database Comparison & Synchronization Tool\r\n\r\n\r\nAvailable Commands:\r\ncompare (compare 2 db schemas):\r\ncompare <dbms> <from connection string> <to connection string>\r\nsync (sync 2 db schemas):\r\nsync <dbms> <from connection string> <to connection string>\r\ndeploy (deploy script to target):\r\ndeploy <dbms> <to connection string> <script to run>\r\n", sb.ToString());
+            Assert.AreEqual($"torque Database Comparison & Synchronization Tool{Environment.NewLine}{Environment.NewLine}{Environment.NewLine}Available Commands:{Environment.NewLine}compare (compare 2 db schemas):{Environment.NewLine}compare <dbms> <from connection string> <to connection string>{Environment.NewLine}sync (sync 2 db schemas):{Environment.NewLine}sync <dbms> <from connection string> <to connection string>{Environment.NewLine}deploy (deploy script to target):{Environment.NewLine}deploy <dbms> <to connection string> <script to run>{Environment.NewLine}", sb.ToString());
         }
     }
 
