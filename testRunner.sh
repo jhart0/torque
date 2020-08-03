9428c633be76a687eb478e2ac81bba6974e6c0a8
Josh Hart <josh.hart@eposnow.com>
2020-08-03 14:18:15 +0100

    Update tests to fail properly

--- testRunner.sh
+++ testRunner.sh
@@ -1,6 +1,6 @@
 while read a; do
   echo "Checking $a Occurs exactly once in diff output"
-  count=$(grep -c "$a" log | wc -l)
+  count=$(grep -o "$a" log | wc -l)
   if [ "$count" -ne "1" ]; then
    echo "Error, $a occured $count times";
    exit 1;
